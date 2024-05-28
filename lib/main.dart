cdaf4237d99aa39f575129cac978d9d439e50504
Hafoza <163436847+real-Hafez@users.noreply.github.com>
2024-05-28 20:27:18 +0300

    sex

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demode',
       theme: ThemeData(
         // This is the ssstheme of your application.
         //
