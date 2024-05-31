fee9be13082726f07a018b1f269f02a10413535b
Hafoza <163436847+real-Hafez@users.noreply.github.com>
2024-05-31 05:48:30 +0300

    change theme + create notes_view

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class notes_view extends StatelessWidget {
+  const notes_view({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
