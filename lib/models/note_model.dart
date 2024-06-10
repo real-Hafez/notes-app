d56f5552240338323f4130330a8fa2c2509da8bf
Hafoza <163436847+real-Hafez@users.noreply.github.com>
2024-06-10 07:20:58 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+class notemodel {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+
+  notemodel({
+    required this.title,
+    required this.subtitle,
+    required this.date,
+    required this.color,
+  });
+}
