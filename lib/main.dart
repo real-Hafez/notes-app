fde9027466d7e9a88a1aa9fc46be1faef053160f
Hafoza <163436847+real-Hafez@users.noreply.github.com>
2024-06-10 10:43:07 +0300

    init hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/views/notes_view.dart';
 
-void main() {
-  runApp(const notes_app());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox('Notes');
+
+  runApp(const NotesApp());
 }
 
-class notes_app extends StatelessWidget {
-  const notes_app({super.key});
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
