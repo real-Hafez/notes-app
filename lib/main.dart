1e6910a4cf19062ea927ffe6e8157cc4fae4608d
Hafoza <163436847+real-Hafez@users.noreply.github.com>
2024-06-12 05:32:43 +0300

    ass simble bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/Simple_Bloc_Observer.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:path_provider/path_provider.dart';
@@ -11,6 +12,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final appDocumentDir = await getApplicationDocumentsDirectory();
   await Hive.initFlutter(appDocumentDir.path);
+  Bloc.observer = SimpleBlocObserver();
   await Hive.openBox('name');
   Hive.registerAdapter(notemodelAdapter());
   runApp(const NotesApp());
@@ -23,7 +25,7 @@ class NotesApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context)=>AddNoteCubit()),
+        BlocProvider(create: (context) => AddNoteCubit()),
       ],
       child: MaterialApp(
         theme: ThemeData(brightness: Brightness.dark),
